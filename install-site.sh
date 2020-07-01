e8394ec4d432b79f49e406481e5221626476aebf
dlb213 <dlb213@gmail.com>
2020-07-01 17:06:27 -0400

    setup input variables, alias setup, sites.php, all needs testing.  next setup the folder copying for doing site config.

--- install-site.sh
+++ install-site.sh
@@ -1,27 +1,56 @@
 #!/bin/bash
 
-#this will just create the db, user, password
-#then
-#spit out the info to a file,
-#probably change the file perms to only 700 chown $USER
+#dbname=$1
 
+site=$1
+short=$2
 
-echo "Remember this only creates the database, user nothing else."
-
-dbname=$1
+#if the user doesn't specify the doc root use casdev
+docroot=$3
 
 # flags
 # -d8
 # -d9
 
-if [ "$dbname" = "" ]
+if [ "$site" = "" ]
 then
     echo "No name provided
-Usage: $0 dbname "
+Usage: $0 <full site url> <shortname> <docroot>"
 exit 1
 fi
 
+if [ "$short" = "" ]
+then
+    echo "No name provided
+Usage: $0 <full site url> <shortname> <docroot>"
+exit 1
+fi
+
+if [ "$docroot" = "" -o "$docroot" = "casdev"]
+then
+    echo "no docroot provided (or you chose to use casdev, the default)
+Usage: $0 <full site url> <shortname> <docroot(d9, d8, or casdev[default])>
+
+Using default of /var/www/casdev/web/
+"
+    rootpath= /var/www/casdev/web
+    cd $rootpath/sites/
+
+    dbprefix="casdev"
+    
+    
+    #else d9
+    #else d8
+    #else exit
+    
+    
+fi
+
+year=$(date +'%y')
+
 pass=$(pwgen -s 16)
+
+dbname="$dbprefix""_$short""_$year"
 
 sudo mysql -e "CREATE DATABASE $dbname"
 sudo mysql -e "CREATE USER '$dbname'@'%' IDENTIFIED BY '$pass'"
@@ -35,7 +64,27 @@ pass: $pass
 mysql://$dbname:$pass@localhost/$dbname
 " >> dbinfo/$dbname.txt
 
-echo "drush site-install standard --account-name=$dbname_admin --account-mail=incasweb@lehigh.edu --site-mail=incasweb@lehigh.edu --account-pass=$(pwgen 16) --site-name='Drupal8 fresh install' --db-url=mysql://$dbname:$pass@localhost/$dbname --sites-subdir="
+#copy folder and stuff
+
+
+
+#add site to sites.php
+echo "\$sites['$site'] = '$short';
+" >> sites.php
+
+#setup alias
+echo "
+$short:
+  root: $rootpath
+  uri: http://$site
+" >> $rootpath/../drush/sites/'$dbprefix'.sites.yml
+
+drush cc drush
+
+drush @'$dbprefix'.$short site-install standard --account-name='$dbname'_cas_admin --account-mail=incasweb@lehigh.edu --site-mail=incasweb@lehigh.edu --account-pass=$(pwgen 16) --site-name='CAS Dev Server $short Site'
+#--db-url=mysql://$dbname:$pass@localhost/$dbname
+
+
 
 #to-do:
 #
@@ -43,3 +92,5 @@ echo "drush site-install standard --account-name=$dbname_admin --account-mail=in
 # add an alias
 # setup base_url
 # setup config_sync_directory
+
+#copy the site folder from somewhere
